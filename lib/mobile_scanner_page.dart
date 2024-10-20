68aa04dc039e33b75d49ddd0eab4d4bd34347b65
Ahmed A. Khalifa <ahmedKhalifa090@gmail.com>
2024-10-20 08:52:58 +0300

    making mods

---
+++ lib/mobile_scanner_page.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'package:qr_checker/config/data.dart';
+
+class MobileScannerPage extends StatefulWidget {
+  const MobileScannerPage({super.key});
+
+  @override
+  State<MobileScannerPage> createState() => _MobileScannerPageState();
+}
+
+class _MobileScannerPageState extends State<MobileScannerPage>
+    with WidgetsBindingObserver {
+  final MobileScannerController controller = MobileScannerController(
+    detectionSpeed: DetectionSpeed.noDuplicates,
+    // required options for the scanner
+  );
+  Barcode? _barcode;
+  StreamSubscription<Object?>? _subscription;
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+
+        _scanUrl(_barcode);
+
+        if (kDebugMode) {
+          print(_barcode?.rawValue);
+        }
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Start listening to lifecycle changes.
+    WidgetsBinding.instance.addObserver(this);
+
+    // Start listening to the barcode events.
+    _subscription = controller.barcodes.listen(_handleBarcode);
+
+    // Finally, start the scanner itself.
+    unawaited(controller.start());
+  }
+
+  @override
+  Future<void> dispose() async {
+    // Stop listening to lifecycle changes.
+    WidgetsBinding.instance.removeObserver(this);
+    // Stop listening to the barcode events.
+    unawaited(_subscription?.cancel());
+    _subscription = null;
+    // Dispose the widget itself.
+    super.dispose();
+    // Finally, dispose of the controller.
+    await controller.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // If the controller is not ready, do not try to start or stop it.
+    // Permission dialogs can trigger lifecycle changes before the controller is ready.
+    if (!controller.value.hasCameraPermission) {
+      return;
+    }
+
+    switch (state) {
+      case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+      case AppLifecycleState.paused:
+        return;
+      case AppLifecycleState.resumed:
+        // Restart the scanner when the app is resumed.
+        // Don't forget to resume listening to the barcode events.
+        _subscription = controller.barcodes.listen(_handleBarcode);
+
+        unawaited(controller.start());
+      case AppLifecycleState.inactive:
+        // Stop the scanner when the app is paused.
+        // Also stop the barcode events subscription.
+        unawaited(_subscription?.cancel());
+        _subscription = null;
+        unawaited(controller.stop());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MobileScanner(
+      controller: controller,
+    );
+  }
+}
+
+Future<void> _scanUrl(Barcode? barcode) async {
+  String? urlEncoded = base64UrlEncode(barcode!.rawBytes!.toList());
+
+  var url = Uri.parse('https://www.virustotal.com/api/v3/urls');
+
+  var body = {'url': urlEncoded};
+
+  var req = http.Request('POST', url);
+  req.headers.addAll(headers);
+  req.bodyFields = body;
+
+  var res = await req.send();
+  final resBody = await res.stream.bytesToString();
+
+  if (res.statusCode >= 200 && res.statusCode < 300) {
+    print(resBody);
+  } else {
+    print(res.reasonPhrase);
+  }
+}
